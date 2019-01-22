a210799fbf45d866f59db565358e61a5f863157c
Sam Wang <samofcorinth@gmx.ca>
2019-01-21 22:14:36 -0500

    Preskaŭ finita

--- src/main/kotlin/xyz/5atm/menteia/datumo/AWSAlirilo.kt
+++ src/main/kotlin/xyz/5atm/menteia/datumo/AWSAlirilo.kt
@@ -1,8 +1,23 @@
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
+import kotlinx.serialization.parse
 import org.dom4j.DocumentHelper
+import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest
+import software.amazon.awssdk.services.lambda.LambdaClient
+import software.amazon.awssdk.services.lambda.model.InvokeRequest
 import software.amazon.awssdk.services.polly.PollyClient
-import software.amazon.awssdk.services.polly.PollyClientBuilder
+import software.amazon.awssdk.services.polly.model.OutputFormat
+import software.amazon.awssdk.services.polly.model.SynthesizeSpeechRequest
+import software.amazon.awssdk.services.polly.model.TextType
+import software.amazon.awssdk.services.polly.model.VoiceId
 import xyz.`5atm`.menteia.vorttrakto.SintaksoArbo
+import java.lang.Exception
+import java.nio.file.FileSystems
+import java.text.SimpleDateFormat
+import java.util.*
 
 internal fun paroli(arbo: SintaksoArbo): Unit {
     val polly = PollyClient.builder()
@@ -12,23 +27,85 @@ internal fun paroli(arbo: SintaksoArbo): Unit {
     val xmlRadiko = xml.addElement("speak")
             .addElement("amazon:effect")
             .addAttribute("vocal-tract-length", "+7%")
-    var vortoj = mutableListOf<String>()
+    val vortoj = mutableListOf<String>()
     arbo.traversi(kunPaŭzoj = true).forEach {
         if (it.startsWith("!")) {
-
+            val ipa = igiIPA(vortoj.joinToString(" "))
+            xmlRadiko.addElement("phoneme")
+                    .addAttribute("alphabet", "ipa")
+                    .addAttribute("ph", ipa.joinToString(" "))
+            when (it) {
+                "!longapaŭzo" -> xmlRadiko.addElement("break")
+                "!paŭzo" -> xmlRadiko.addElement("break").addAttribute("strength", "weak")
+            }
+            vortoj.clear()
         }
+    }
+    val petoXML = xml.asXML()
+    polly.synthesizeSpeech(SynthesizeSpeechRequest.builder()
+            .outputFormat(OutputFormat.OGG_VORBIS)
+            .text(petoXML)
+            .voiceId(VoiceId.IVY)
+            .textType(TextType.SSML)
+            .build(),
+            FileSystems.getDefault().getPath(
+                    "ekparolado",
+                    "${SimpleDateFormat("yyyy-MM-dd HHmmss").format(Calendar.getInstance().time)}.ogg"
+            )
+    )
+
+}
+
+@Serializable
+data class Peto(val IPA: String)
+
+@Serializable
+data class Respondenhavo(
+        val ĈuFinita: Boolean = false,
+        val Kialo: String = "",
+        val IPA: List<String> = listOf()
+)
+
+private fun igiIPA(vortoj: String): List<String> {
+    val lambda = LambdaClient.builder()
+            .region(Region.US_EAST_1)
+            .build()
+    val respondo = lambda.invoke(InvokeRequest.builder()
+            .functionName("Menteia-Kontrolilo")
+            .payload(SdkBytes.fromUtf8String("{\"IPA\":\"$vortoj\"}"))
+            .build()
+    )
+    val enhavo = JSON.parse(Respondenhavo.serializer(), respondo.payload().asUtf8String())
+    if (enhavo.ĈuFinita) {
+        return enhavo.IPA
+    } else {
+        throw Exception(enhavo.Kialo)
     }
 }
 
-private fun igiIPA(vortoj: Array<String>): List<String> {
-    val IPA = mutableListOf<String>()
-    vortoj.forEach {
-        when (it) {
-            "sagi" -> IPA.add("sə'gi")
-            else -> {
+data class Vorto(val vorto: String, val valenco: Int, val antaŭpaŭzo: Boolean, val interpaŭzo: Boolean)
 
-            }
+object Vortaro {
+    private val vortaro: MutableMap<String, Vorto> = mutableMapOf()
+
+    fun alporti(): Map<String, Vorto> {
+        if (vortaro.isNotEmpty()) {
+            return vortaro
         }
+        val db = DynamoDbClient.builder()
+                .region(Region.US_WEST_2)
+                .build()
+        val respondo = db.scan(ScanRequest.builder().tableName("Menteia-vortaro").build())
+        respondo.items().forEach {
+            val vorto = it["vorto"]!!.s()
+            val datumo = Vorto(
+                    vorto = vorto,
+                    valenco = it["valenco"]!!.n().toInt(),
+                    antaŭpaŭzo = it["antaŭpaŭzo"]?.bool() ?: false,
+                    interpaŭzo = it["interpaŭzo"]?.bool() ?: false
+            )
+            vortaro[vorto] = datumo
+        }
+        return vortaro
     }
-    return IPA
 }
--- src/main/kotlin/xyz/5atm/menteia/vorttrakto/SintaksoArbo.kt
+++ src/main/kotlin/xyz/5atm/menteia/vorttrakto/SintaksoArbo.kt
@@ -1,16 +1,42 @@
 package xyz.`5atm`.menteia.vorttrakto
 
-internal data class SintaksoArbo(val radiko: String, val opcioj: List<SintaksoArbo>) {
+import java.lang.Exception
+
+data class SintaksoArbo(val radiko: String, val opcioj: List<SintaksoArbo>) {
+    companion object {
+        fun konstrui(
+                frazo: String,
+                vortoj: Iterator<String> = frazo.splitToSequence(" "
+                ).iterator()): SintaksoArbo {
+            if (!vortoj.hasNext()) {
+                throw Exception("$frazo ne estas valida")
+            }
+            val sekva = vortoj.next()
+            val vortaro = Vortaro.alporti()
+            val vorto = vortaro[sekva] ?: throw Exception("$sekva ne estas en la vortaro")
+            val valenco = vorto.valenco
+            return SintaksoArbo(sekva, (0 until valenco).map {
+                konstrui(frazo, vortoj)
+            })
+        }
+    }
+
     fun traversi(kunPaŭzoj: Boolean = false): Sequence<String> {
         return sequence {
+            val vorto = Vortaro.alporti()[radiko]
+            if (vorto == null) {
+                throw Exception("$radiko ne estas en la vortaro")
+            }
+            if (kunPaŭzoj and vorto.antaŭpaŭzo) {
+                yield("!longapaŭzo")
+            }
             yield(radiko)
             opcioj.forEach {
+                if (kunPaŭzoj and vorto.interpaŭzo) {
+                    yield("!paŭzo")
+                }
                 yieldAll(it.traversi())
             }
         }
     }
-}
-
-internal fun silaboj(vorto: String): List<String> {
-
 }
